2b2affa1cbb72c94205bf32aef60f656456a3f6d
suryarive <surya.prakash@rive.ai>
2026-03-01 15:19:17 +0530

    add vercel.json for SPA routing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -83,7 +83,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.eejrd7f273k"
+    "revision": "0.98q8dnjj6vk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
